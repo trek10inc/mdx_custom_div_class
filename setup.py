dc7a66280fe8eee20287bec76adf97790085ebd2
Jared Short <jaredlshort@gmail.com>
2015-11-30 14:32:55 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 
 from setuptools import setup
 setup(
-    name='mdx_customspanclass',
-    version='1.1.1',
-    author='Konrad Wasowicz',
-    author_email='exaroth@gmail.com',
+    name='mdx_customdivclass',
+    version='1.0.0',
+    author='Jared Short (forked from Konrad Wasowicz)',
+    author_email='jshort@trek10.com',
     description='Markdown extension which allows inserting span elements with custom class',
-    url='https://github.com/exaroth/mdx_custom_span_class',
-    py_modules=['mdx_custom_span_class'],
+    url='https://github.com/trek10inc/mdx_custom_div_class',
+    py_modules=['mdx_custom_div_class'],
     install_requires=['Markdown>=2.0',],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 1 - Beta',
         'Operating System :: OS Independent',
         'License :: OSI Approved :: BSD License',
         'Intended Audience :: Developers',
